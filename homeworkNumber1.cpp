3575a2a23f1fea99c78438116c478d6aabbe1791
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-29 08:34:35 +0300

    Create homeworkNumber1.cpp

---
+++ homeworkNumber1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+int main(){
+   int n;
+   std::cout << "Введите число" << std::endl;
+   std::cin >> n;
+   int middle = fmod(n, 100) / 10;
+   int first = n / 100;
+   n = middle * 100 + first * 10 + fmod(n, 10);
+   std::cout << n << std::endl;
+   return 0;
+}
