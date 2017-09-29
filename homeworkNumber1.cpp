f40626ea8942193299eed0132bce3f4b1213dd25
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-09-29 08:55:35 +0300

    Update homeworkNumber1.cpp

--- homeworkNumber1.cpp
+++ homeworkNumber1.cpp
@@ -6,10 +6,8 @@ int main(){
     std::cout << "Введите число" << std::endl;
     int n;
     std::cin >> n;
-    int middle;
-    middle = fmod(n, 100) / 10;
-    int first;
-    first = n / 100;
+    int middle = fmod(n, 100) / 10;
+    int first = n / 100;
     n = middle * 100 + first * 10 + fmod(n, 10);
     std::cout << n << std::endl;
     return 0;
